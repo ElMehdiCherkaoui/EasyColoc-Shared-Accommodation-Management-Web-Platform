6b019f6148ce0598dc50c5b49fe11c136745e0c1
ElMehdiCherkaoui <cherkaouim451@gmail.com>
2026-02-25 11:47:58 +0000

    feat : improvment in ui and remove uneeded pages

--- EasyColoc-Shared-Accommodation-Management-Web-Platform/resources/views/layouts/app.blade.php
+++ EasyColoc-Shared-Accommodation-Management-Web-Platform/resources/views/layouts/app.blade.php
@@ -48,25 +48,7 @@
                 Users & Moderation
             </a>
 
-            <a href="{{ route('admin.accommodations.index') }}"
-                class="flex items-center gap-3 px-3 py-2.5 rounded-lg font-medium transition-colors
-               {{ request()->routeIs('admin.accommodations.*') ? 'bg-indigo-500/10 text-indigo-400' : 'hover:bg-slate-800 hover:text-white' }}">
-                <svg class="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                        d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
-                </svg>
-                Accommodations
-            </a>
 
-            <a href="{{ route('admin.expenses.index') }}"
-                class="flex items-center gap-3 px-3 py-2.5 rounded-lg font-medium transition-colors
-               {{ request()->routeIs('admin.expenses.*') ? 'bg-indigo-500/10 text-indigo-400' : 'hover:bg-slate-800 hover:text-white' }}">
-                <svg class="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                        d="M9 14l6-6m-5.5.5h.01m4.99 5h.01M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16l3.5-2 3.5 2 3.5-2 3.5 2zM10 8.5a.5.5 0 11-1 0 .5.5 0 011 0zm5 5a.5.5 0 11-1 0 .5.5 0 011 0z" />
-                </svg>
-                Global Expenses
-            </a>
 
             <a href="{{ route('dashboard') }}"
                 class="flex items-center gap-3 px-3 py-2.5 rounded-lg font-medium transition-colors 'hover:bg-slate-800 hover:text-white' }}">
